8d688e18547e814693de0a18b396a62c87a32692
iAmMe27 <98499526+iAmMe27@users.noreply.github.com>
2025-05-02 17:48:34 +0100

    fixed vanity image
    added auto-text colour settings

---
+++ Launcher.UI/ContrastColourConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace iAmModlist_Launcher.Launcher.UI
+{
+    class ContrastColourConverter
+    {
+        public static Color GetAverageColour(UIElement element, Rect region)
+        {
+            var rtb = new RenderTargetBitmap((int)region.Width, (int)region.Height, 96, 96, PixelFormats.Pbgra32);
+            var dv = new DrawingVisual();
+
+            using (var dc = dv.RenderOpen())
+            {
+                var vb = new VisualBrush(element);
+                dc.DrawRectangle(vb, null, new Rect(new Point(), region.Size));
+            }
+
+            rtb.Render(dv);
+
+            var pixels = new byte[(int)(region.Width * region.Height * 4)];
+            rtb.CopyPixels(pixels, (int)region.Width * 4, 0);
+
+            long r = 0, g = 0, b = 0;
+            int pixelCount = (int)(region.Width * region.Height);
+
+            for (int i = 0; i < pixels.Length; i += 4)
+            {
+                b += pixels[i];
+                g += pixels[i + 1];
+                r += pixels[i + 2];
+            }
+
+            return Color.FromRgb((byte)(r / pixelCount), (byte)(g / pixelCount), (byte)(b / pixelCount));
+        }
+
+        public static Brush GetContrastingTextBrush(Color backgroundColor)
+        {
+            double brightness = 0.299 * backgroundColor.R + 0.587 * backgroundColor.G + 0.114 * backgroundColor.B;
+            return brightness > 128 ? Brushes.Black : Brushes.White;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,14 +1,14 @@
-﻿using System.Text;
+﻿
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using iAmModlist_Launcher.Launcher.Settings;
 using iAmModlist_Launcher.Launcher.Customisation;
+using iAmModlist_Launcher.Launcher.UI;
 using ModernWpf;
 using System.ComponentModel;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows.Controls;
 
 namespace iAmModlist_Launcher
 {
@@ -105,6 +105,16 @@ namespace iAmModlist_Launcher
             InitializeComponent();
 
             _ = SettingsInitializer();
+            SizeChanged += MainWindow_SizeChanged;
+        }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double newHeight = e.NewSize.Height;
+            double newWidth = e.NewSize.Width;
+
+            VanityImage.Width = newWidth;
+            VanityImage.Height = newHeight;
         }
 
         private async Task SettingsInitializer()
@@ -121,6 +131,11 @@ namespace iAmModlist_Launcher
             Theme = customisationSettings?.Theme;
             AccentColour = customisationSettings?.AccentColour;
             BackgroundImage = customisationSettings?.BackgroundImage;
+             
+            var pathForVanityImage = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, BackgroundImage);
+            VanityImage.Source = new BitmapImage(new Uri(pathForVanityImage, UriKind.Absolute));
+            VanityImage.Width = 900;
+            VanityImage.Height = 450;
 
             ThemeManager.Current.ApplicationTheme = Theme switch
             {
@@ -134,13 +149,30 @@ namespace iAmModlist_Launcher
                 "Purple" => Color.FromArgb(0xFF, 0x8A, 0x2B, 0xE2),
                 "Blue" => Color.FromArgb(0xFF, 0x00, 0x00, 0xFF),
                 "Green" => Color.FromArgb(0xFF, 0x00, 0xFF, 0x00),
+                "Red" => Color.FromArgb(0xFF, 0xFF, 0x00, 0x00),
+                "Yellow" => Color.FromArgb(0xFF, 0xFF, 0xFF, 0x00),
+                "Orange" => Color.FromArgb(0xFF, 0xFF, 0xA5, 0x00),
                 _ => Color.FromArgb(0xFF, 0x8A, 0x2B, 0xE2)
             };
         }
+
         public event PropertyChangedEventHandler? PropertyChanged;
 
         protected void OnPropertyChange(string name) =>
                         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        private void ModlistName_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBlock tb && VanityImage != null)
+            {
+                // Determine where the text appears relative to the Image
+                var location = tb.TransformToVisual(VanityImage).Transform(new Point(0, 0));
+                var rect = new Rect(location, new Size(tb.ActualWidth, tb.ActualHeight));
+
+                var color = ContrastColourConverter.GetAverageColour(VanityImage, rect);
+                tb.Foreground = ContrastColourConverter.GetContrastingTextBrush(color);
+            }
+        }
     }
 }
 
